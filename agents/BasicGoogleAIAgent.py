a29330c456ec93e62e9f728caff0dff08222b80c
ATHOS ARNOUR LEAL <athosarnourleal@gmail.com>
2026-07-15 22:08:50 -0300

    Add files via upload

---
+++ agents/BasicGoogleAIAgent.py
@@ -0,0 +1,75 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.messages import SystemMessage,HumanMessage,AIMessage,ToolMessage, AnyMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+class BasicGoogleGenAIAgent:
+
+    def __init__(self,
+        name: str,
+        basic_instructions: str,
+        tools: list,
+        model: str= "gemini-2.5-flash"
+    ) -> None:
+
+        self.NAME = name
+        self.SYSTEM_PROMPT = SystemMessage(content=basic_instructions)
+
+        self.llm = ChatGoogleGenerativeAI(
+            model= model,
+            api_key=os.getenv("GOOGLE_API_KEY")
+        )
+
+        self.token_usage = 0
+
+        if len(tools) > 0:
+            self.llm_with_tools = self.llm.bind_tools(tools=tools)
+            self.tools = {t.name : t for t in tools}
+        else:
+            self.llm_with_tools = None
+
+    def formatToolResponse(self, tool_message: ToolMessage) -> AnyMessage:
+        """formatação que pode ser customizada pelas classes filho"""
+        return tool_message
+
+    def invoke(self,
+        query: str,
+        context: list | str | None = None
+    ) -> AIMessage:
+        """invoke llm either as a simple text-only call, or as a call based on tool responses"""
+
+        # gather messages
+        messages: list = [self.SYSTEM_PROMPT]
+        if context is str:
+            messages.append(SystemMessage(content=str(context)))
+        elif context is not None:
+            for message in context:
+                messages.append(message)
+        messages.append(HumanMessage(content=query))
+
+        # text only invoke
+        if self.llm_with_tools is None:
+            text_only_response = self.llm.invoke(messages)
+            self.token_usage += text_only_response.usage_metadata["total_tokens"]
+            return text_only_response
+
+        # get answer with tools available
+        initial_llm_response = self.llm_with_tools.invoke(messages)
+        self.token_usage += initial_llm_response.usage_metadata["total_tokens"]
+
+        if len(initial_llm_response.tool_calls) > 0:
+            # execute tools
+            for i, call in enumerate(initial_llm_response.tool_calls):
+                tool_response: ToolMessage = self.tools[call["name"]].invoke(call)
+
+                messages.append(self.formatToolResponse(tool_response))
+
+            final_llm_response = self.llm.invoke(messages)
+            self.token_usage += final_llm_response.usage_metadata["total_tokens"]
+
+            return final_llm_response
+
+        return initial_llm_response
---
+++ agents/MainAgent.py
@@ -0,0 +1,98 @@
+from langchain_core.messages import AnyMessage, ToolMessage, SystemMessage
+
+from agents.BasicGoogleAIAgent import BasicGoogleGenAIAgent
+from langchain_tavily import TavilySearch
+
+import json
+from docs.TraceManager import addToTraceJson
+
+# setup tavily tool
+search_tool = TavilySearch(
+    max_results= 3,
+    include_url= True,
+    include_usage=True,
+    search_depth= "advanced",
+    chunks_per_source= 3
+)
+
+class MainAgent(BasicGoogleGenAIAgent):
+
+    def __init__(self):
+        super().__init__(
+            name="Main Agent",
+            basic_instructions=
+            """
+you are a question answering agent,
+your job is to receive a context and a question(query) and answer the question based 
+ONLY on the given context and the web search results
+
+#STEPS:
+1. check if the CONTEXT or the CONTEXT FROM WEB SEARCH has the information needed to respond the query and construct your answer ONLY from them
+2. in cases where you are NOT ABLE answer the query you must:
+    - if available, you must call the web search tool
+    - if the web search tool is unavailable, you must try to explain why you could not answer the query
+3. if possible to answer(even partially) the query, you must then answer it.
+
+RULES:
+- when possible to answer question, you NEED to include source citations
+- you must include NO extras(like signatures), ONLY the answer and the source citations
+- do not include markdown fences
+- answer must have the same language as the query
+            """,
+            tools=[search_tool]
+        )
+        self.numberOfToolResponses = 0
+
+    # noinspection PyTypeChecker
+    def formatToolResponse(self, tool_message: ToolMessage) -> AnyMessage:
+        """transformar os resultados do tavily search tool para um formato que poderia ser lido e interpretado mais facilmente"""
+
+        raw_tool_content = json.loads(tool_message.content)
+
+        raw_tool_content.update({"reason_for_call":"answer not found in original query"})
+
+        # extract only results
+        page_data = raw_tool_content["results"]
+
+        formated_tool_content = "CONTEXT FROM WEB SEARCH:\n"
+        for page in page_data:
+            formated_data = f"\nweb page title:{page["title"]}\ncontents:\n{page["content"]}\nWEB PAGE SOURCE: {page["url"]}\n\n"
+            formated_tool_content+=formated_data
+
+        addToTraceJson({"formatted_tool_message": {"agent that called": self.NAME, "content": formated_tool_content, "usage": raw_tool_content["usage"]}})
+
+        return SystemMessage(content= formated_tool_content)
+
+    def answerQuery(self, query: str, context: str | list):
+
+        response = self.invoke(
+            query= query,
+            context= f"CONTEXT: {context}"
+        )
+
+        response_content: str = str(response.content)
+
+        if response_content[:1] == "[": # if response is a list[dict]
+
+            # format to json and extract
+            response_content = response_content.replace("\'", "\"")
+
+            try:
+                content_dict = json.loads(response_content)[0]
+
+                addToTraceJson({"main_agent_response": content_dict['text']})
+
+                return content_dict['text']
+            except json.JSONDecodeError:
+                print("MAIN AGENT ERROR IN LOADS(RESPONSE_CONTENT)(JSONDecodeError)")
+                print(response_content)
+            except Exception:
+                print("MAIN AGENT ERROR IN LOADS(RESPONSE_CONTENT)(GENERAL EXCEPTION)")
+                print(response_content)
+        else:
+            # response is a simple string
+
+            addToTraceJson({"main_agent_response": response_content})
+            return response_content
+
+main_agent = MainAgent()
