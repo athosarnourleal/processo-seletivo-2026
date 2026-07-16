caba6c9ba850d68ee6bd5a103604d4f544f09c8e
ATHOS ARNOUR LEAL <athosarnourleal@gmail.com>
2026-07-16 17:16:03 -0300

    Add files via upload

--- docs/TraceManager.py
+++ docs/TraceManager.py
@@ -8,19 +8,42 @@ def addToTraceJson(
         clear: bool = False
     ) -> None:
 
-    # solve path
     file_path = folder_path / "trace.json"
 
-    if file_path.exists() and clear is False and os.stat(file_path).st_size > 0:
-        with open(file_path, "r") as file:
-            try:
-                cur_trace = json.load(file)
-            except json.decoder.JSONDecodeError:
-                cur_trace: dict = {}
-    else:
-        cur_trace: dict = {}
+    cur_trace:dict = {}
+    if not clear:
+        cur_trace = loadTraceJson(folder_path=folder_path, file_name="trace.json")
 
     cur_trace.update(addition)
 
     with open(file_path, "w") as file:
         json.dump(cur_trace, file, indent= 4)
+
+def loadTraceJson(
+        folder_path: Path = Path(__file__).parent.parent / "docs",
+        file_name: str = "trace.json"
+    ):
+
+    file_path = folder_path / file_name
+
+    if isTraceValid(folder_path= folder_path, file_name= file_name):
+        with open(file_path, "r") as file:
+            try:
+                cur_trace = json.load(file)
+            except json.decoder.JSONDecodeError:
+                cur_trace = {}
+
+        return cur_trace
+
+    return {}
+
+def isTraceValid(
+        folder_path: Path = Path(__file__).parent.parent / "docs",
+        file_name: str = "trace.json"
+) -> bool:
+    file_path = folder_path / file_name
+
+    if file_path.exists() and os.stat(file_path).st_size > 0:
+        return True
+    else:
+        return False
